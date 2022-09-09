a4386098fbdd9b5878cb6f1a70694ca27bb11cbc
Bianka Zieba <bianka.zieba@gmail.com>
2022-09-09 18:50:23 +0200

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app>
     <v-main>
+      <Navbar />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  }),
+  data: () => ({}),
+  components: { Navbar },
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout>
+    <v-app-bar density="compact" absolute>
+      <v-app-bar-title>TodoApp</v-app-bar-title>
+
+      <v-btn to="/todos">Todos</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-divider inset vertical></v-divider>
+
+      <v-btn
+        class="mr-4 ml-5"
+        color="primary"
+        variant="outlined"
+        href="https://github.com/biankazieba"
+        plain
+      >
+        <v-icon left icon="mdi-github"></v-icon>
+
+        <span>Bianka Zieba</span>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-card height="50px"></v-card>
+    </v-main>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({}),
+};
+</script>
