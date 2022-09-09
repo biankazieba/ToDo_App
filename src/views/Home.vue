ded867a31a4808cd41fed524e8be910bdc9addc2
Bianka Zieba <bianka.zieba@gmail.com>
2022-09-09 18:55:01 +0200

    Remove "HelloWord" component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
-  <div>
-    <HelloWorld />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld.vue";
-
 export default {
   name: "Home",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
