106895317a9de0388113744a1dbd3c682fecbf84
Bianka Zieba <bianka.zieba@gmail.com>
2022-09-10 13:07:23 +0200

    Add Todo view

---
+++ src/views/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+  <TodoList />
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  name: "Todos",
+
+  components: { Navbar, TodoList },
+};
+</script>
