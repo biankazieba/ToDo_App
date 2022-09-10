49719bc2a8c8290b16c308f76dd58f39474ce7f3
Bianka Zieba <bianka.zieba@gmail.com>
2022-09-10 12:20:13 +0200

    Change navbar to views

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <Navbar />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue";
-
 export default {
   name: "App",
 
   data: () => ({}),
-  components: { Navbar },
 };
 </script>
